8ee1a449a09d400873589afaa89978c6a3df4c35
richmondafari <afaririchmond1000@gmail.com>
2025-01-02 20:54:18 +0000

    Build the components of the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import MainLayout from "./layouts/MainLayout";
+import JobsPage from "./pages/JobsPage";
+import NotFound from "./pages/NotFound";
 
-import NavBar from "./components/NavBar";
-import Hero from "./components/Hero";
-import HomeCards from "./components/HomeCards";
-import JobListings from "./components/JobListings";
-import ViewAllJobs from "./ViewAllJobs";
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path="/jobs" element={<JobsPage />} />
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+);
+
 
 const App = () => {
-  return (
-    <>
-      <NavBar />
-      <Hero title="This is the Title" subTitle="Subtitle of props" />
-      <HomeCards />
-      <JobListings />
-      <ViewAllJobs />
-
-     
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
